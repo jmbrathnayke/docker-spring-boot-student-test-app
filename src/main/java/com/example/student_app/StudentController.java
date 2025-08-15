0c3adffa480a032eeb4e3800221ec5fc5aeeb7bc
Manujayaa <janithrathnayake01@gmail.com>
2025-08-15 23:09:12 +0530

    webapp initialized

---
+++ src/main/java/com/example/student_app/StudentController.java
@@ -0,0 +1,21 @@
+package com.example.student_app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    StudentRepo repo;
+
+    @RequestMapping("/getStudents")
+    public List<Student> getStudents(){
+       return  repo.findAll(); //return the data for database
+
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Student(name,age) VALUES ('MANUJAYA',22);
+INSERT INTO Student(name,age) VALUES ('BANDA',23);
+INSERT INTO Student(name,age) VALUES ('JAN',24);
+INSERT INTO Student(name,age) VALUES ('kapi',25);
